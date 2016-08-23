119dfb66e7c451fdbd69328fac5e86e1a88ad9ae
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-08-23 15:20:46 +0900

    updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# ovpd-fms
+
+Financial management system for the University of the Philippines Office of the Vice President for Development.
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,6 @@
 
 require 'roo'
 
-User.create!([
-	{:first_name => "John Thomas Raphael", :last_name => "Dulay", :email => "johnthomasraphael@gmail.com", :password => "pass1234", :password_confirmation => "pass1234" }
-	])
 
 Category.create!([
 		{:description => "MOOE", :positive_balance => true },
